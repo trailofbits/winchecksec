794d3dee2b67ac5893313a31c516f03653e01bdf
William Woodruff <william.woodruff@trailofbits.com>
2020-05-22 17:07:47 -0400

    treewide: Unified version, Doxygen build (#74)
    * treewide: Unified version, Doxygen build
    
    * Checksec: Fill in docs, more impl isolation
    
    * github/workflows: Auto documentation deployment
    
    * workflows/docs: Use a more recent actions-gh-pages
    
    * workflows/docs: Drop ww/docs from docs-deploy
    
    * util: Add release helper
    
    * Update Checksec.h
    
    Co-authored-by: Eric Kilmer <eric.d.kilmer@gmail.com>
    
    Co-authored-by: Eric Kilmer <eric.d.kilmer@gmail.com>

--- Checksec.h
+++ Checksec.h
@@ -27,6 +27,20 @@ using json = nlohmann::json;
 
 namespace checksec {
 
+/**
+ * Raised on Checksec instantiation in the event of malformed or invalid PE.
+ */
+class EXPORT ChecksecError : public std::runtime_error {
+   public:
+    ChecksecError(const char* what) : std::runtime_error(what) {}
+};
+
+/**
+ * A namespace for winchecksec's implementation internals.
+ *
+ * Members of this namespace are not part of the public API.
+ */
+namespace impl {
 constexpr const char kDynamicBaseDescription[] =
     "Binaries with dynamic base support can be "
     "dynamically rebased, enabling ASLR.";
@@ -76,26 +90,9 @@ constexpr const char kGSDescription[] =
 constexpr const char kDotNETDescription[] =
     ".NET binaries run in a managed environment with many default mitigations.";
 
-enum class EXPORT MitigationPresence {
-    Present,
-    NotPresent,
-    NotApplicable,
-    NotImplemented,
-};
-
-class EXPORT ChecksecError : public std::runtime_error {
-   public:
-    ChecksecError(const char* what) : std::runtime_error(what) {}
-};
-
-struct EXPORT MitigationReport {
-    MitigationPresence presence;
-    std::string description;
-    std::optional<std::string> explanation;
-
-    operator bool() const { return presence == MitigationPresence::Present; }
-};
-
+/**
+ * A RAII wrapped for `pe-parse::parsed_pe`.
+ */
 class LoadedImage {
    public:
     explicit LoadedImage(const std::string path) {
@@ -114,32 +111,140 @@ class LoadedImage {
    private:
     peparse::parsed_pe* pe_;
 };
+}  // namespace impl
 
+/**
+ * Models the state of a security mitigation.
+ *
+ * Every mitigation supported by winchecksec is in one of these states.
+ */
+enum class EXPORT MitigationPresence {
+    Present,        /**< The mitigation is present */
+    NotPresent,     /**< The mitigation is not present */
+    NotApplicable,  /**< The mitigation is not applicable on this input */
+    NotImplemented, /**< Support for detecting this mitigation is not implemented */
+};
+
+/**
+ * Represents a "report" on a particular security mitigation.
+ */
+struct EXPORT MitigationReport {
+    /**
+     * A MitigationPresence indicating the mitigation's state.
+     */
+    MitigationPresence presence;
+
+    /**
+     * A brief description of the mitigation.
+     */
+    std::string description;
+
+    /**
+     * An optional explanation of the mitigation's detection (or non-detection).
+     */
+    std::optional<std::string> explanation;
+
+    /**
+     * @return true if `presence` is \ref MitigationPresence::Present, false otherwise
+     */
+    operator bool() const { return presence == MitigationPresence::Present; }
+};
+
+/**
+ * Represents the main winchecksec interface.
+ */
 class EXPORT Checksec {
    public:
     Checksec(std::string filepath);
 
     json toJson() const;
 
+    /**
+     * @return a MitigationReport indicating whether the program can be loaded from a dynamic base
+     *  address (i.e. `/DYNAMICBASE`)
+     */
     const MitigationReport isDynamicBase() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program has **effective** ASLR
+     *  (i.e., has a dynamic base and unstripped relocations, or is in a managed runtime like .NET)
+     */
     const MitigationReport isASLR() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program supports 64-bit ASLR
+     */
     const MitigationReport isHighEntropyVA() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program's integrity
+     *  must be checked at load time
+     */
     const MitigationReport isForceIntegrity() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program supports
+     *  NX, (a.k.a. DEP, `W^X`)
+     */
     const MitigationReport isNX() const;
+
+    /**
+     * @return a MitigationReport indicating whether the operating system
+     *  should attempt a manifest lookup and load for the program
+     */
     const MitigationReport isIsolation() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program uses Structured Exception Handlers
+     */
     const MitigationReport isSEH() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program supports Control Flow Guard
+     */
     const MitigationReport isCFG() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program contains a (partially) valid
+     *  Authenticode signature
+     *
+     * @note See the [`uthenticode`](https://trailofbits.github.io/uthenticode/index.html)
+     *       documentation for the details of this check
+     */
     const MitigationReport isAuthenticode() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program supports Return Flow Guard
+     */
     const MitigationReport isRFG() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program supports safe SEH
+     */
     const MitigationReport isSafeSEH() const;
+
+    /**
+     * @return a MitigationReport indicating whether the program uses stack buffer cookies
+     *  (a.k.a. stack guards, stack canaries)
+     *
+     * @note This check tests for the presence of the security cookie's address and **not**
+     *       the instrumentation that actually checks that address. Every modern version of
+     *       MSVCRT has stack cookies enabled in some form, so this can result in false
+     *       positives if your application code links to MSVCRT but doesn't enable its own
+     *       stack cookies.
+     */
     const MitigationReport isGS() const;
+
+    /**
+     * @return a MitigationReport indicating whether this program runs in the .NET environment
+     */
     const MitigationReport isDotNET() const;
 
     operator json() const;
     friend std::ostream& operator<<(std::ostream& os, Checksec&);
 
    private:
-    LoadedImage loadedImage_;
+    impl::LoadedImage loadedImage_;
     std::string filepath_;
     std::uint16_t targetMachine_ = 0;
     std::uint16_t imageCharacteristics_ = 0;
