289f53a377c41e194b6d25b09449edc6e9180e34
William Woodruff <william@trailofbits.com>
2020-05-26 13:19:26 -0400

    treewide: More unit test work

--- test/winchecksec-test.cpp
+++ test/winchecksec-test.cpp
@@ -1,1 +1,133 @@
+#include "gtest/gtest.h"
+
 #include <checksec.h>
+
+TEST(Winchecksec, NoDynamicBase32) {
+    auto *path = WINCHECKSEC_TEST_ASSETS "/32/pegoat-no-dynamicbase.exe";
+
+    auto checksec = checksec::Checksec(path);
+
+    EXPECT_FALSE(checksec.isDynamicBase());
+    EXPECT_FALSE(checksec.isASLR());
+}
+
+TEST(Winchecksec, NoDynamicBase64) {
+    auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat-no-dynamicbase.exe";
+
+    auto checksec = checksec::Checksec(path);
+
+    EXPECT_FALSE(checksec.isDynamicBase());
+    EXPECT_FALSE(checksec.isASLR());
+}
+
+TEST(Winchecksec, NoHighEntropyVA32) {
+    // NOTE: 32-bit programs obviously don't support 64-bit ASLR.
+    auto *path = WINCHECKSEC_TEST_ASSETS "/32/pegoat.exe";
+
+    auto checksec = checksec::Checksec(path);
+
+    EXPECT_FALSE(checksec.isHighEntropyVA());
+    EXPECT_TRUE(checksec.isASLR());
+}
+
+TEST(Winchecksec, HighEntropyVA64) {
+    // By default, 64-bit PEs use their full address space for ASLR.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_TRUE(checksec.isHighEntropyVA());
+        EXPECT_TRUE(checksec.isASLR());
+    }
+
+    // ...but it can be disabled.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat-no-highentropyva.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_FALSE(checksec.isHighEntropyVA());
+        EXPECT_TRUE(checksec.isASLR());
+    }
+}
+
+TEST(Winchecksec, NX32) {
+    // By default, modern PEs support NX/DEP.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/32/pegoat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_TRUE(checksec.isNX());
+    }
+
+    // ...but it can be disabled.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/32/pegoat-no-nxcompat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_FALSE(checksec.isNX());
+    }
+}
+
+TEST(Winchecksec, NX64) {
+    // By default, modern PEs support NX/DEP.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_TRUE(checksec.isNX());
+    }
+
+    // ...but it can be disabled.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat-no-nxcompat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_FALSE(checksec.isNX());
+    }
+}
+
+TEST(Winchecksec, CFG32) {
+    // By default, modern PEs support CFG.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/32/pegoat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_TRUE(checksec.isCFG());
+    }
+
+    // ...but it can be disabled.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/32/pegoat-no-cfg.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_FALSE(checksec.isCFG());
+    }
+}
+
+TEST(Winchecksec, CFG64) {
+    // By default, modern PEs support CFG.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_TRUE(checksec.isCFG());
+    }
+
+    // ...but it can be disabled.
+    {
+        auto *path = WINCHECKSEC_TEST_ASSETS "/64/pegoat-no-cfg.exe";
+
+        auto checksec = checksec::Checksec(path);
+
+        EXPECT_FALSE(checksec.isCFG());
+    }
+}
